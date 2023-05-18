a9152b4f57b6a12b24b3ca1498a70e73412b1349
DobleTreinta <anthonyovie15gmail.com>
2023-05-17 22:51:02 -0500

    Creacion interfaz repositorio Donante

--- donaton-api/src/main/java/pe/edu/upao/donatonapi/model/Donante.java
+++ donaton-api/src/main/java/pe/edu/upao/donatonapi/model/Donante.java
@@ -11,10 +11,10 @@ import lombok.*;
 public class Donante {
 
     @Getter @Setter
-    @EqualsAndHashCode.Include
+    //@EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private long id;
 
     @Getter @Setter
     @Column(length = 50, nullable = false)
